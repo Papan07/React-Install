949f4e0ed68c47e5e3f219094654dc5ba8df5d1b
Papan07 <papanns19@gmail.com>
2025-03-28 13:27:16 +0530

    react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Papan Namasudra</h1>
+      <p>Email: papan.namasudra.s89@kalvium.community
+      </p>
+      <p> instagram: @papan_079</p>
+      <p>Address: Godavari Global University..</p>
+    </>
+  )
+}
+
+export default App
